ffd0c35baf5fe0d5a055e792b18356c1568ab5ee
Enola Sengeissen <tuife@hotmail.fr>
2020-05-18 15:48:00 +0200

    maj init et plugin

--- veloroutes_voies_vertes/init.py
+++ veloroutes_voies_vertes/init.py
@@ -7,3 +7,20 @@ Created on Mon May 18 15:35:03 2020
 """
 
 
+__copyright__ = "Copyright 2020, 3Liz"
+__license__ = "GPL version 3"
+__email__ = "info@3liz.org"
+__revision__ = "$Format:%H$"
+
+
+# noinspection PyPep8Naming
+def classFactory(iface):  # pylint: disable=invalid-name
+    """Load Plugin
+
+    :param iface: A QGIS interface instance.
+    :type iface: QgsInterface
+    """
+    #
+    from .plugin import VeloroutesPlugin
+
+    return VeloroutesPlugin(iface)
