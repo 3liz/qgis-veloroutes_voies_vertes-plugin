cd8294a022b7130e3676e2c3a3dada18a52f3996
Enola-sengeissen <65539832+Enola-sengeissen@users.noreply.github.com>
2020-05-20 10:50:56 +0200

    initialisation du plugin  (#1)
    * [test] ajout metadata
    
    * inititalisation outils plugin
    
    * init.py
    
    * ajout fichier plugin
    
    * maj init et plugin
    
    * adaptation metadata.txt
    
    * Update veloroutes_voies_vertes/init.py
    
    Co-authored-by: Étienne Trimaille <gustrimaille@yahoo.fr>
    
    * Update veloroutes_voies_vertes/metadata.txt
    
    Co-authored-by: Étienne Trimaille <gustrimaille@yahoo.fr>
    
    * Update veloroutes_voies_vertes/metadata.txt
    
    Co-authored-by: Étienne Trimaille <gustrimaille@yahoo.fr>
    
    * Update veloroutes_voies_vertes/metadata.txt
    
    Co-authored-by: Étienne Trimaille <gustrimaille@yahoo.fr>
    
    * Update veloroutes_voies_vertes/metadata.txt
    
    Co-authored-by: Étienne Trimaille <gustrimaille@yahoo.fr>
    
    * Update veloroutes_voies_vertes/metadata.txt
    
    Co-authored-by: Étienne Trimaille <gustrimaille@yahoo.fr>
    
    * Update veloroutes_voies_vertes/metadata.txt
    
    Co-authored-by: Étienne Trimaille <gustrimaille@yahoo.fr>
    
    * Update veloroutes_voies_vertes/plugin.py
    
    Co-authored-by: Étienne Trimaille <gustrimaille@yahoo.fr>
    
    Co-authored-by: Enola Sengeissen <tuife@hotmail.fr>
    Co-authored-by: Étienne Trimaille <gustrimaille@yahoo.fr>

---
+++ veloroutes_voies_vertes/init.py
@@ -0,0 +1,24 @@
+"""
+Created on Mon May 18 15:35:03 2020
+
+@author: enolasengeissen
+"""
+
+
+__copyright__ = "Copyright 2020, 3Liz"
+__license__ = "GPL version 3"
+__email__ = "info@3liz.org"
+__revision__ = "$Format:%H$"
+
+
+# noinspection PyPep8Naming
+def classFactory(iface):  # pylint: disable=invalid-name
+    """Load Plugin
+
+    :param iface: A QGIS interface instance.
+    :type iface: QgsInterface
+    """
+    #
+    from .plugin import VeloroutesPlugin
+
+    return VeloroutesPlugin(iface)
---
+++ veloroutes_voies_vertes/plugin.py
@@ -0,0 +1,27 @@
+"""
+Created on Mon May 18 15:38:08 2020
+
+@author: enolasengeissen
+"""
+__copyright__ = "Copyright 2020, 3Liz"
+__license__ = "GPL version 3"
+__email__ = "info@3liz.org"
+__revision__ = "$Format:%H$"
+
+
+from qgis.core import QgsApplication
+
+class VeloroutesPlugin:
+    def __init__(self, iface):
+        self.iface = iface
+        self.provider = None
+        
+    def initProcessing (self):
+        pass
+    
+    def initGui(self):
+        self.initProcessing()
+        
+    def unload(self):
+        if self.provider:
+            QgsApplication.processingRegistry().removeProvider(self.provider)
