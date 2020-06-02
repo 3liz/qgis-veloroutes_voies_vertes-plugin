7914ae2e4e1360e4adad6848d5945ae4d75dce21
Enola-S <enola.sengeissen@ensg.eu>
2020-06-02 15:10:04 +0200

    add test directory

---
+++ veloroutes_voies_vertes/test/test_functions_without_data.py
@@ -0,0 +1,31 @@
+"""
+Created on Tue Jun  2 14:56:02 2020
+
+@author: enolasengeissen
+
+Tests for load structure with an empty database.
+"""
+
+from .base_test_database import DatabaseWithoutDataTestCase
+
+__copyright__ = "Copyright 2019, 3Liz"
+__license__ = "GPL version 3"
+__email__ = "info@3liz.org"
+__revision__ = "$Format:%H$"
+
+
+class TestSqlFunctions(DatabaseWithoutDataTestCase):
+
+    def test_to_be_removed_later(self):
+        """ Test the database is provided without data. """
+        # Number of tables
+        self.cursor.execute(
+            "SELECT COUNT(*) FROM information_schema.tables WHERE table_schema = 'veloroutes';"
+        )
+        self.assertEqual(21, self.cursor.fetchone()[0])
+
+        # Number of features
+        self.cursor.execute(
+            "SELECT COUNT(*) FROM veloroutes.segment;"
+        )
+        self.assertEqual(0, self.cursor.fetchone()[0])
