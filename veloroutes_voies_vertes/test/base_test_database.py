6e43d14b726dcca2473b32cf1053231ae3c5f104
Enola-sengeissen <65539832+Enola-sengeissen@users.noreply.github.com>
2020-06-03 09:16:27 +0200

    add test directory (#13)
    * add test directory
    
    * add travis.yml
    
    * add init file in test
    
    * rectifications veloroutesprovider instead of adresseprovider
    
    * temporary deleted AS integer

---
+++ veloroutes_voies_vertes/test/base_test_database.py
@@ -0,0 +1,79 @@
+"""
+Created on Tue Jun  2 10:38:36 2020
+
+@author: enolasengeissen
+
+Base class for tests using a database.
+"""
+import psycopg2
+import time
+
+from qgis.core import (
+    QgsApplication,
+    Qgis,
+)
+from qgis.testing import unittest
+
+if Qgis.QGIS_VERSION_INT >= 30800:
+    from qgis import processing
+else:
+    import processing
+
+from ..qgis_plugin_tools.tools.logger_processing import LoggerProcessingFeedBack
+from ..processing.provider import VeloroutesProvider as ProcessingProvider
+
+__copyright__ = "Copyright 2020, 3Liz"
+__license__ = "GPL version 3"
+__email__ = "info@3liz.org"
+__revision__ = "$Format:%H$"
+
+
+class DatabaseTestCase(unittest.TestCase):
+
+    """Base class for tests using a database with data."""
+
+    def __init__(self, methodName="runTest"):
+        super().__init__(methodName)
+        self.cursor = None
+        self.connection = None
+        self.provider = None
+        self.add_data = True
+
+    def setUp(self) -> None:
+        self.connection = psycopg2.connect(
+            user="docker", password="docker", host="db", port="5432", database="gis"
+        )
+        self.cursor = self.connection.cursor()
+
+        self.provider = ProcessingProvider()
+        QgsApplication.processingRegistry().addProvider(self.provider)
+
+        self.feedback = LoggerProcessingFeedBack()
+
+        params = {
+            "CONNECTION_NAME": "test",
+            "OVERRIDE": True,
+            # "ADD_TEST_DATA": self.add_data,
+        }
+        processing.run(
+            "{}:create_database_structure".format(self.provider.id()),
+            params,
+            feedback=None,
+        )
+
+        super().setUp()
+
+    def tearDown(self) -> None:
+        del self.cursor
+        del self.connection
+        time.sleep(1)
+        super().tearDown()
+
+
+class DatabaseWithoutDataTestCase(DatabaseTestCase):
+
+    def __init__(self, methodName="runTest"):
+        super().__init__(methodName)
+        self.add_data = False
+
+
