1e89597928983d74cdc07e18385f3339f22c8933
Enola-sengeissen <enola.sengeissen@ensg.eu>
2020-06-08 09:43:21 +0200

    suggested rectifications

--- veloroutes_voies_vertes/test/test_data.py
+++ veloroutes_voies_vertes/test/test_data.py
@@ -14,25 +14,25 @@ __revision__ = "$Format:%H$"
 
 class TestSqlFunctions(DatabaseTestCase):
 
-    def test_trigger1(self):
+    def test_trigger_repere_numero_serie(self):
         """ Test the trigger on repere(numero_serie)"""
         # Contrainte sur le champs repere(numero_serie) avec la fonction numserie()
         with self.assertRaisesRegex(Exception, 'numero_serie ne peut être NULL si type_noeud vaut CPT'):
             self.cursor.execute(
                 "INSERT INTO veloroutes.repere(type_noeud) VALUES ('CPT')")
 
-    def test_trigger2a(self):
+    def test_trigger_segment_projet_revetement(self):
         """ Test the trigger on segment(revetement) case project"""
         # Contraintes sur le champs segment(revetement) avec la fonction revet()
-        with self.assertRaisesRegex(Exception,
-                                    'revetement ne peut pas prendre de valeur si avancement vaut 1'):
+        with self.assertRaisesRegex(
+                Exception, 'revetement ne peut pas prendre de valeur si avancement vaut 1'):
             self.cursor.execute(
                 "INSERT INTO veloroutes.segment(avancement, revetement, statut) VALUES (1,'LIS','VV')")
 
-    def test_trigger2b(self):
+    def test_trigger_segment_fictif_revetement(self):
         """ Test the trigger on segment(revetement) case fictive geometry"""
-        with self.assertRaisesRegex(Exception,
-                                    'revetement ne peut pas prendre de valeur si la geom est fictive'):
+        with self.assertRaisesRegex(
+                Exception, 'revetement ne peut pas prendre de valeur si la geom est fictive'):
             self.cursor.execute(
                 "INSERT INTO veloroutes.segment(geometrie_fictive, revetement, statut)"
                 " VALUES ('T','LIS','VV')")
