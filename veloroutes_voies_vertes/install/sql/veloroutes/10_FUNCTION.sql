7829faa0f2d6716013f15a24a8b08832f9583815
Enola-sengeissen <65539832+Enola-sengeissen@users.noreply.github.com>
2020-06-08 09:58:36 +0200

    test data (#23)
    * add test data
    
    * Modification of revet() function
    Script that tests trigger functions on insert queries
    
    * gitignore
    
    * suggested rectifications

--- veloroutes_voies_vertes/install/sql/veloroutes/10_FUNCTION.sql
+++ veloroutes_voies_vertes/install/sql/veloroutes/10_FUNCTION.sql
@@ -36,12 +36,17 @@ COMMENT ON FUNCTION veloroutes.numserie() IS 'Empêche que le numéro de série 
 -- revet()
 CREATE FUNCTION veloroutes.revet() RETURNS trigger
     LANGUAGE plpgsql
-    AS $$    BEGIN
+    AS $$
+    BEGIN
         IF NEW.avancement = 1 THEN
-            NEW.revetement := NULL;
+			IF NEW.revetement IS NOT NULL THEN
+            	RAISE EXCEPTION 'revetement ne peut pas prendre de valeur si avancement vaut 1';
+			END IF;
         END IF;
         IF NEW.geometrie_fictive ='T' THEN
-            NEW.revetement := NULL;
+            IF NEW.revetement IS NOT NULL THEN
+            	RAISE EXCEPTION 'revetement ne peut pas prendre de valeur si la geom est fictive';
+			END IF;
         END IF;
         RETURN NEW;
     END;
