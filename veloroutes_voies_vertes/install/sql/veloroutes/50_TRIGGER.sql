b55e9e90ac605505d6e967f5d63935db80918575
Enola-sengeissen <enola.sengeissen@ensg.eu>
2020-07-08 11:54:23 +0200

    renamed trigger functions insert(2) in insert_v_portion(itineraire)

--- veloroutes_voies_vertes/install/sql/veloroutes/50_TRIGGER.sql
+++ veloroutes_voies_vertes/install/sql/veloroutes/50_TRIGGER.sql
@@ -15,20 +15,20 @@ SET check_function_bodies = false;
 SET client_min_messages = warning;
 SET row_security = off;
 
--- v_portion insert
-CREATE TRIGGER insert INSTEAD OF INSERT ON veloroutes.v_portion FOR EACH ROW EXECUTE PROCEDURE veloroutes.v_portion_insert();
+-- v_itineraire insert_v_itineraire
+CREATE TRIGGER insert_v_itineraire INSTEAD OF INSERT ON veloroutes.v_itineraire FOR EACH ROW EXECUTE PROCEDURE veloroutes.v_itineraire_insert();
 
 
--- TRIGGER insert ON v_portion
-COMMENT ON TRIGGER insert ON veloroutes.v_portion IS 'Rend la vue éditable avec la fonction v_portion_insert()';
+-- TRIGGER insert_v_itineraire ON v_itineraire
+COMMENT ON TRIGGER insert_v_itineraire ON veloroutes.v_itineraire IS 'Rend la vue éditable avec la fonction v_itineraire_insert()';
 
 
--- v_itineraire insert2
-CREATE TRIGGER insert2 INSTEAD OF INSERT ON veloroutes.v_itineraire FOR EACH ROW EXECUTE PROCEDURE veloroutes.v_itineraire_insert();
+-- v_portion insert_v_portion
+CREATE TRIGGER insert_v_portion INSTEAD OF INSERT ON veloroutes.v_portion FOR EACH ROW EXECUTE PROCEDURE veloroutes.v_portion_insert();
 
 
--- TRIGGER insert2 ON v_itineraire
-COMMENT ON TRIGGER insert2 ON veloroutes.v_itineraire IS 'Rend la vue éditable avec la fonction v_itineraire_insert()';
+-- TRIGGER insert_v_portion ON v_portion
+COMMENT ON TRIGGER insert_v_portion ON veloroutes.v_portion IS 'Rend la vue éditable avec la fonction v_portion_insert()';
 
 
 -- repere numserie
