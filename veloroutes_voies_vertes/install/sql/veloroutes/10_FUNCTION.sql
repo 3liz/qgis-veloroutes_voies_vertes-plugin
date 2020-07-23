b623b1f68212965391ec21a2803d5243c69471f2
Enola-sengeissen <enola.sengeissen@ensg.eu>
2020-07-23 09:59:19 +0200

    diffs to pass test_migration

--- veloroutes_voies_vertes/install/sql/veloroutes/10_FUNCTION.sql
+++ veloroutes_voies_vertes/install/sql/veloroutes/10_FUNCTION.sql
@@ -26,7 +26,7 @@ CREATE FUNCTION veloroutes.insert_in_veloroutes(table_name text) RETURNS boolean
 		--id_local,
 		--id_on3v,
 		statut,
-		avancement, 
+		avancement,
 		revetement,
 		proprietaire,
 		gestionnaire,
@@ -34,23 +34,23 @@ CREATE FUNCTION veloroutes.insert_in_veloroutes(table_name text) RETURNS boolean
 		src_geom,
 		sens_unique,
 		date_saisie)
-		SELECT 
-		geom, 
+		SELECT
+		geom,
 		--id_local,
 		--id_on3v,
 		statut,
 		CAST (avancement AS INTEGER),
 		revetement,
-		proprietaire, 
-		gestionnaire, 
+		proprietaire,
+		gestionnaire,
 		precision,
 		src_geom,
 		sens_unique,
-		CASE 
+		CASE
 			WHEN substring(date_saisie from 1 for 10) LIKE '__-__-____' THEN to_date(substring(date_saisie from 1 for 10),'DD-MM-YYYY')
 			WHEN substring(date_saisie from 1 for 10) LIKE '__/__/____' THEN to_date(substring(date_saisie from 1 for 10),'DD-MM-YYYY')
 		END AS date_saisie
-		
+
 		FROM imports.import_segment
 		WHERE avancement IS NOT null
 		AND statut IS NOT null
@@ -58,7 +58,7 @@ CREATE FUNCTION veloroutes.insert_in_veloroutes(table_name text) RETURNS boolean
 		AND EXISTS (SELECT 1 FROM veloroutes.etat_avancement_val WHERE code = CAST (avancement AS INTEGER))
 		AND EXISTS (SELECT 1 FROM veloroutes.statut_segment_val WHERE code = statut)
 		AND EXISTS (SELECT 1 FROM veloroutes.revetement_val WHERE code = revetement);
-		
+
 		RAISE NOTICE 'segment a été importé dans veloroutes';
 	END IF;
 
