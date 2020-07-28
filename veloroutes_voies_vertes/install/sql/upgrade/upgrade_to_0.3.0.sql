3e0b067ed1e2c241154b9567d65e00b4ad7de0b2
Enola-sengeissen <enola.sengeissen@ensg.eu>
2020-07-28 16:33:56 +0200

    1 insert trigger for each table + every fields are taen into account

--- veloroutes_voies_vertes/install/sql/upgrade/upgrade_to_0.3.0.sql
+++ veloroutes_voies_vertes/install/sql/upgrade/upgrade_to_0.3.0.sql
@@ -1,9 +1,148 @@
-CREATE FUNCTION veloroutes.insert_in_veloroutes(table_name text) RETURNS boolean
+-- insert_veloroutes_itineraire()
+CREATE FUNCTION veloroutes.insert_veloroutes_itineraire() RETURNS boolean
     LANGUAGE plpgsql
     AS $$BEGIN
-    IF table_name = 'segment' THEN
+	INSERT INTO veloroutes.itineraire(
+		site_web,
+		numero,
+		nom_usage,
+		nom_officiel,
+		niveau_schema,
+		est_inscrit,
+		depart,
+		arrivee,
+		annee_inscription,
+		annee_subv,
+		mont_subv,
+		annee_ouverture)
+	SELECT
+		site_web,
+		numero,
+		nom_usage,
+		nom_officiel,
+		CASE
+			WHEN EXISTS (SELECT 1 FROM veloroutes.niveau_administratif_val WHERE code = niveau_schema)
+			THEN niveau_schema
+			WHEN EXISTS (SELECT 1 FROM veloroutes.niveau_administratif_val WHERE libelle = niveau_schema)
+			THEN (SELECT code FROM veloroutes.niveau_administratif_val as v WHERE v.libelle = niveau_schema LIMIT 1)
+			ELSE niveau_schema
+		END AS niveau_schema,
+		CASE
+			WHEN EXISTS (SELECT 1 FROM veloroutes.booleen_val WHERE code = est_inscrit)
+			THEN est_inscrit
+			WHEN EXISTS (SELECT 1 FROM veloroutes.booleen_val WHERE libelle = est_inscrit)
+			THEN (SELECT code FROM veloroutes.booleen_val as v WHERE v.libelle = est_inscrit LIMIT 1)
+			--WHEN est_inscrit = 'non' THEN 'F'
+			--WHEN est_inscrit = 'oui' THEN 'T'
+			ELSE est_inscrit
+		END AS est_inscrit,
+		depart,
+		arrivee,
+		CASE
+			WHEN substring(annee_inscription from 1 for 10) LIKE '__-__-____' THEN to_date(substring(annee_inscription from 1 for 10),'DD-MM-YYYY')
+			WHEN substring(annee_inscription from 1 for 10) LIKE '__/__/____' THEN to_date(substring(annee_inscription from 1 for 10),'DD-MM-YYYY')
+		END AS annee_inscription,
+		CASE
+			WHEN substring(annee_subv from 1 for 10) LIKE '__-__-____' THEN to_date(substring(annee_subv from 1 for 10),'DD-MM-YYYY')
+			WHEN substring(annee_subv from 1 for 10) LIKE '__/__/____' THEN to_date(substring(annee_subv from 1 for 10),'DD-MM-YYYY')
+		END AS annee_subv,
+		CAST (mont_subv AS real),
+		CASE
+			WHEN substring(annee_ouverture from 1 for 10) LIKE '__-__-____' THEN to_date(substring(annee_ouverture from 1 for 10),'DD-MM-YYYY')
+			WHEN substring(annee_ouverture from 1 for 10) LIKE '__/__/____' THEN to_date(substring(annee_ouverture from 1 for 10),'DD-MM-YYYY')
+		END AS annee_ouverture
+	FROM imports.import_itineraire
+	WHERE numero IS NOT NULL
+	AND (EXISTS (SELECT 1 FROM veloroutes.niveau_administratif_val WHERE code = niveau_schema)
+		OR EXISTS (SELECT 1 FROM veloroutes.niveau_administratif_val WHERE libelle = niveau_schema)
+		OR niveau_schema IS NULL)
+	AND (EXISTS (SELECT 1 FROM veloroutes.booleen_val WHERE code = est_inscrit)
+		 OR EXISTS (SELECT 1 FROM veloroutes.booleen_val WHERE libelle = est_inscrit)
+		 OR est_inscrit IS NULL);
 
-		INSERT INTO veloroutes.segment(
+	RAISE NOTICE 'Les lignes correctes de itineraire ont été importées dans veloroutes';
+	RETURN 1;
+END$$;
+
+
+-- insert_veloroutes_portion()
+CREATE FUNCTION veloroutes.insert_veloroutes_portion() RETURNS boolean
+    LANGUAGE plpgsql
+    AS $$BEGIN
+
+	--creation des tables element et etape
+    -- import_element
+    CREATE TABLE imports.import_element(
+		id serial,
+        id_portion integer,
+        id_segment integer);
+     -- import_etape
+    CREATE TABLE imports.import_etape(
+		id serial,
+        id_portion integer,
+        id_itineraire integer);
+
+     -- remplissage de import_etape
+	INSERT INTO imports.import_etape(
+        id_portion,
+        id_itineraire)
+    SELECT
+		id_import,
+        lien_itin
+    FROM imports.import_portion
+    WHERE imports.import_portion.lien_itin IS NOT NULL;
+
+     -- remplissage de element_import
+    INSERT INTO imports.import_element(
+		id_portion,
+        id_segment)
+	SELECT
+        id_import,
+		lien_segm
+    FROM imports.import_portion
+    WHERE imports.import_portion.lien_segm IS NOT NULL;
+
+    -- adaptation et insertion des portions dans véloroutes
+	INSERT INTO veloroutes.portion(
+		type_portion,
+		mont_subv,
+		annee_subv,
+		id_local,
+		id_on3v,
+		nom,
+		description
+		)
+	SELECT
+		CASE
+			WHEN EXISTS (SELECT 1 FROM veloroutes.portion_val WHERE code = type_portion)
+			THEN type_portion
+			WHEN EXISTS (SELECT 1 FROM veloroutes.portion_val WHERE libelle = type_portion)
+			THEN (SELECT code FROM veloroutes.portion_val as v WHERE v.libelle = type_portion LIMIT 1)
+		END AS type_portion,
+		CAST (mont_subv AS real),
+		CASE
+			WHEN substring(annee_subv from 1 for 10) LIKE '__-__-____' THEN to_date(substring(annee_subv from 1 for 10),'DD-MM-YYYY')
+			WHEN substring(annee_subv from 1 for 10) LIKE '__/__/____' THEN to_date(substring(annee_subv from 1 for 10),'DD-MM-YYYY')
+		END AS annee_subv,
+		id_local,
+		id_on3v,
+		nom,
+		description
+	FROM imports.import_portion
+	WHERE type_portion IS NOT NULL
+	AND (EXISTS (SELECT 1 FROM veloroutes.portion_val WHERE code = type_portion)
+		OR EXISTS (SELECT 1 FROM veloroutes.portion_val WHERE libelle = type_portion));
+
+	RAISE NOTICE 'Les lignes correctes de portion ont été importées dans veloroutes';
+	RETURN 1;
+END$$;
+
+
+-- insert_veloroutes_segment()
+CREATE FUNCTION veloroutes.insert_veloroutes_segment() RETURNS boolean
+    LANGUAGE plpgsql
+    AS $$BEGIN
+	INSERT INTO veloroutes.segment(
 		geom,
 		id_local,
 		id_on3v,
@@ -15,8 +154,11 @@ CREATE FUNCTION veloroutes.insert_in_veloroutes(table_name text) RETURNS boolean
 		precision,
 		src_geom,
 		sens_unique,
-		date_saisie)
-		SELECT
+		date_saisie,
+		src_annee,
+		geometrie_fictive,
+		annee_ouverture)
+	SELECT
 		CASE
 			WHEN ST_SRID(geom) != 2154 THEN ST_Transform(ST_SetSRID(geom,2154),2154)
 			ELSE geom
@@ -58,171 +200,40 @@ CREATE FUNCTION veloroutes.insert_in_veloroutes(table_name text) RETURNS boolean
 		CASE
 			WHEN substring(date_saisie from 1 for 10) LIKE '__-__-____' THEN to_date(substring(date_saisie from 1 for 10),'DD-MM-YYYY')
 			WHEN substring(date_saisie from 1 for 10) LIKE '__/__/____' THEN to_date(substring(date_saisie from 1 for 10),'DD-MM-YYYY')
-		END AS date_saisie
-
-		FROM imports.import_segment
-		WHERE avancement IS NOT null
-		AND statut IS NOT null
-		-- check that enumerate types are correct
-		AND (EXISTS (SELECT 1 FROM veloroutes.etat_avancement_val WHERE code = CAST (avancement AS INTEGER))
-			 OR EXISTS (SELECT 1 FROM veloroutes.etat_avancement_val WHERE libelle = avancement))
-		AND (EXISTS (SELECT 1 FROM veloroutes.statut_segment_val WHERE code = statut)
-			 OR EXISTS (SELECT 1 FROM veloroutes.statut_segment_val WHERE libelle = statut))
-		AND (EXISTS (SELECT 1 FROM veloroutes.revetement_val WHERE code = revetement)
-			 OR EXISTS (SELECT 1 FROM veloroutes.revetement_val WHERE libelle = revetement)
-			 OR revetement IS NULL)
-		AND (EXISTS (SELECT 1 FROM veloroutes.booleen_val WHERE code = sens_unique)
-			 OR EXISTS (SELECT 1 FROM veloroutes.booleen_val WHERE libelle = sens_unique)
-			 OR sens_unique IS NULL)
-		;
-
-		RAISE NOTICE 'Les lignes correctes de segment ont été importées dans veloroutes';
-	END IF;
-
-	IF table_name = 'portion' THEN
-
-        --creation des tables element et etape
-        -- import_element
-        CREATE TABLE imports.import_element(
-            id integer NOT NULL,
-            id_portion integer,
-            id_segment integer);
-        -- import_element_id_seq
-        CREATE SEQUENCE imports.import_element_id_seq
-            AS integer
-            START WITH 1
-            INCREMENT BY 1
-            NO MINVALUE
-            NO MAXVALUE
-            CACHE 1;
-        -- import_element_id_seq
-        ALTER SEQUENCE imports.import_element_id_seq OWNED BY imports.import_element.id;
-        ALTER TABLE ONLY imports.import_element ALTER COLUMN id SET DEFAULT nextval('imports.import_element_id_seq'::regclass);
-
-        -- import_etape
-        CREATE TABLE imports.import_etape(
-            id_portion integer,
-            id_itineraire integer,
-            id integer NOT NULL);
-        -- import_etape_id_seq
-        CREATE SEQUENCE imports.import_etape_id_seq
-            AS integer
-            START WITH 1
-            INCREMENT BY 1
-            NO MINVALUE
-            NO MAXVALUE
-            CACHE 1;
-        -- import_etape_id_seq
-        ALTER SEQUENCE imports.import_etape_id_seq OWNED BY imports.import_etape.id;
-        ALTER TABLE ONLY imports.import_etape ALTER COLUMN id SET DEFAULT nextval('imports.import_etape_id_seq'::regclass);
-
-        -- remplissage de import_etape
-        INSERT INTO imports.import_etape(
-        id_portion,
-        id_itineraire)
-        SELECT
-        id_import,
-        lien_itin
-        FROM imports.import_portion
-        WHERE imports.import_portion.lien_itin IS NOT NULL;
-
-        -- remplissage de element_import
-        INSERT INTO imports.import_element(
-        id_portion,
-        id_segment)
-        SELECT
-        id_import,
-        lien_segm
-        FROM imports.import_portion
-        WHERE imports.import_portion.lien_segm IS NOT NULL;
-
-        -- adaptation et insertion des portions dans véloroutes
-		INSERT INTO veloroutes.portion(
-		type_portion,
-		mont_subv,
-		annee_subv
-		)
-		SELECT
+		END AS date_saisie,
+		src_annee,
 		CASE
-			WHEN EXISTS (SELECT 1 FROM veloroutes.portion_val WHERE code = type_portion)
-			THEN type_portion
-			WHEN EXISTS (SELECT 1 FROM veloroutes.portion_val WHERE libelle = type_portion)
-			THEN (SELECT code FROM veloroutes.portion_val as v WHERE v.libelle = type_portion LIMIT 1)
-		END AS type_portion,
-		CAST (mont_subv AS real),
+			WHEN EXISTS (SELECT 1 FROM veloroutes.booleen_val WHERE code = geometrie_fictive)
+			THEN geometrie_fictive
+			WHEN EXISTS (SELECT 1 FROM veloroutes.booleen_val WHERE libelle = geometrie_fictive)
+			THEN (SELECT code FROM veloroutes.booleen_val as v WHERE v.libelle = geometrie_fictive LIMIT 1)
+			ELSE geometrie_fictive
+		END AS geometrie_fictive,
 		CASE
-			WHEN substring(annee_subv from 1 for 10) LIKE '__-__-____' THEN to_date(substring(annee_subv from 1 for 10),'DD-MM-YYYY')
-			WHEN substring(annee_subv from 1 for 10) LIKE '__/__/____' THEN to_date(substring(annee_subv from 1 for 10),'DD-MM-YYYY')
-		END AS annee_subv
-		FROM imports.import_portion
-		WHERE type_portion IS NOT NULL
-		AND (EXISTS (SELECT 1 FROM veloroutes.portion_val WHERE code = type_portion)
-			 OR EXISTS (SELECT 1 FROM veloroutes.portion_val WHERE libelle = type_portion));
+			WHEN substring(annee_ouverture from 1 for 10) LIKE '__-__-____' THEN to_date(substring(annee_ouverture from 1 for 10),'DD-MM-YYYY')
+			WHEN substring(annee_ouverture from 1 for 10) LIKE '__/__/____' THEN to_date(substring(annee_ouverture from 1 for 10),'DD-MM-YYYY')
+		END AS annee_ouverture
+	FROM imports.import_segment
+	WHERE avancement IS NOT null
+	AND statut IS NOT null
+	-- check that enumerate types are correct
+	AND (EXISTS (SELECT 1 FROM veloroutes.etat_avancement_val WHERE code = CAST (avancement AS INTEGER))
+		 OR EXISTS (SELECT 1 FROM veloroutes.etat_avancement_val WHERE libelle = avancement))
+	AND (EXISTS (SELECT 1 FROM veloroutes.statut_segment_val WHERE code = statut)
+		 OR EXISTS (SELECT 1 FROM veloroutes.statut_segment_val WHERE libelle = statut))
+	AND (EXISTS (SELECT 1 FROM veloroutes.revetement_val WHERE code = revetement)
+		 OR EXISTS (SELECT 1 FROM veloroutes.revetement_val WHERE libelle = revetement)
+		 OR revetement IS NULL)
+	AND (EXISTS (SELECT 1 FROM veloroutes.booleen_val WHERE code = sens_unique)
+		 OR EXISTS (SELECT 1 FROM veloroutes.booleen_val WHERE libelle = sens_unique)
+		 OR sens_unique IS NULL)
+	AND (EXISTS (SELECT 1 FROM veloroutes.booleen_val WHERE code = geometrie_fictive)
+		 OR EXISTS (SELECT 1 FROM veloroutes.booleen_val WHERE libelle = geometrie_fictive)
+		 OR geometrie_fictive IS NULL);
 
-		RAISE NOTICE 'Les lignes correctes de portion ont été importées dans veloroutes';
-	END IF;
-
-	IF table_name = 'itineraire' THEN
-
-		INSERT INTO veloroutes.itineraire(
-			site_web,
-			numero,
-			nom_usage,
-			nom_officiel,
-			niveau_schema,
-			est_inscrit,
-			depart,
-			arrivee,
-			annee_inscription,
-			annee_subv,
-			mont_subv)
-		SELECT
-			site_web,
-			numero,
-			nom_usage,
-			nom_officiel,
-			CASE
-				WHEN EXISTS (SELECT 1 FROM veloroutes.niveau_administratif_val WHERE code = niveau_schema)
-				THEN niveau_schema
-				WHEN EXISTS (SELECT 1 FROM veloroutes.niveau_administratif_val WHERE libelle = niveau_schema)
-				THEN (SELECT code FROM veloroutes.niveau_administratif_val as v WHERE v.libelle = niveau_schema LIMIT 1)
-				ELSE niveau_schema
-			END AS niveau_schema,
-			CASE
-				WHEN EXISTS (SELECT 1 FROM veloroutes.booleen_val WHERE code = est_inscrit)
-				THEN est_inscrit
-				WHEN EXISTS (SELECT 1 FROM veloroutes.booleen_val WHERE libelle = est_inscrit)
-				THEN (SELECT code FROM veloroutes.booleen_val as v WHERE v.libelle = est_inscrit LIMIT 1)
-				--WHEN est_inscrit = 'non' THEN 'F'
-				--WHEN est_inscrit = 'oui' THEN 'T'
-				ELSE est_inscrit
-			END AS est_inscrit,
-			depart,
-			arrivee,
-			CASE
-				WHEN substring(annee_inscription from 1 for 10) LIKE '__-__-____' THEN to_date(substring(annee_inscription from 1 for 10),'DD-MM-YYYY')
-				WHEN substring(annee_inscription from 1 for 10) LIKE '__/__/____' THEN to_date(substring(annee_inscription from 1 for 10),'DD-MM-YYYY')
-			END AS annee_inscription,
-			CASE
-				WHEN substring(annee_subv from 1 for 10) LIKE '__-__-____' THEN to_date(substring(annee_subv from 1 for 10),'DD-MM-YYYY')
-				WHEN substring(annee_subv from 1 for 10) LIKE '__/__/____' THEN to_date(substring(annee_subv from 1 for 10),'DD-MM-YYYY')
-			END AS annee_subv,
-			CAST (mont_subv AS real)
-		FROM imports.import_itineraire
-		WHERE numero IS NOT NULL
-		AND (EXISTS (SELECT 1 FROM veloroutes.niveau_administratif_val WHERE code = niveau_schema)
-			OR EXISTS (SELECT 1 FROM veloroutes.niveau_administratif_val WHERE libelle = niveau_schema)
-			OR niveau_schema IS NULL)
-		AND (EXISTS (SELECT 1 FROM veloroutes.booleen_val WHERE code = est_inscrit)
-			 OR EXISTS (SELECT 1 FROM veloroutes.booleen_val WHERE libelle = est_inscrit)
-			 OR est_inscrit IS NULL);
-
-
-		RAISE NOTICE 'Les lignes correctes de itineraire ont été importées dans veloroutes';
-	END IF;
-
-	RETURN TRUE;
-END;$$;
+	RAISE NOTICE 'Les lignes correctes de segment ont été importées dans veloroutes';
+	RETURN 1;
+END$$;
 
 CREATE TABLE veloroutes.booleen_val (
     code text,
